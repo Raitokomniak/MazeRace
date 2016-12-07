982bbca270cd19f58676a42e061e0b7ba5f19a56
Raitokomniak <raitokomniak@gmail.com>
2016-12-07 12:40:42 +0200

    Sokkelogeneraatio muokattavalla koolla tehty

---
+++ Assets/Scripts/Environment/Cell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell {
+	public bool visited;
+
+	public GameObject north;
+	public GameObject south;
+	public GameObject west;
+	public GameObject east;
+
+	public Cell(){
+		north = null;
+		south = null;
+		west = null;
+		east = null;
+	}
+}
---
+++ Assets/Scripts/Environment/MazeGenerator.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class MazeGenerator : MonoBehaviour {
+
+	public GameObject wall;
+	public float wallLength = 1f;
+	public float xSize = 5f;
+	public float ySize = 5f;
+
+	Vector3 initialPos;
+	public GameObject wallHolder;
+	public Cell[] cells;
+	public int currentCell = 0;
+	int totalCells;
+	int visitedCells = 0;
+	bool startedBuilding = false;
+	int currentNeighbour = 0;
+	List<int> lastCells;
+	int backingUp = 0;
+	int wallToBreak;
+	// Use this for initialization
+	void Start () {
+
+		//Muuta tämä sitten triggeröitymään eri paikasta kun peli alkaa
+		CreateWalls ();
+	}
+
+	void CreateWalls(){
+		initialPos = new Vector3 ((-xSize / 2) + wallLength/2, 0, (-ySize/2)+wallLength/2);
+		Vector3 myPos = initialPos;
+		GameObject tempWall;
+
+		//X axis
+		for (int i = 0; i < ySize; i++) {
+			for (int j = 0; j <= xSize; j++) {
+				myPos = new Vector3 (initialPos.x + (j * wallLength) - wallLength / 2, 0, initialPos.z + (i * wallLength) - wallLength / 2);
+				tempWall = Instantiate (wall, myPos, Quaternion.identity) as GameObject;
+				tempWall.transform.SetParent (wallHolder.transform);
+			}
+		}
+
+		//Y axis
+		for (int i = 0; i <= ySize; i++) {
+			for (int j = 0; j < xSize; j++) {
+				myPos = new Vector3 (initialPos.x + (j * wallLength), 0, initialPos.z + (i * wallLength) - wallLength);
+				tempWall = Instantiate (wall, myPos, Quaternion.Euler(0,90,0)) as GameObject;
+				tempWall.transform.SetParent (wallHolder.transform);
+			}
+		}
+
+		CreateCells ();
+	}
+
+	void CreateCells(){
+		lastCells = new List<int>();
+		lastCells.Clear ();
+		totalCells = Mathf.RoundToInt(xSize * ySize);
+		GameObject[] allWalls;
+		int children = wallHolder.transform.childCount;
+		allWalls = new GameObject [children];
+		cells = new Cell[Mathf.RoundToInt(xSize) * Mathf.RoundToInt(ySize)];
+
+		int eastWestProcess = 0;
+		int childProcess = 0;
+		int termCount = 0;
+
+		for (int i = 0; i < children; i++) {
+			allWalls [i] = wallHolder.transform.GetChild (i).gameObject;
+		}
+
+		for (int cellProcess = 0; cellProcess < cells.Length; cellProcess++) {
+			cells [cellProcess] = new Cell ();
+			cells [cellProcess].east = allWalls [eastWestProcess];
+			cells [cellProcess].south = allWalls [Mathf.RoundToInt(childProcess + (xSize+1) * ySize)];
+			if (termCount == xSize) {
+				eastWestProcess += 2;
+				termCount = 0;
+			} else
+				eastWestProcess++;
+			termCount++; 
+			childProcess++;
+			cells [cellProcess].west = allWalls [eastWestProcess];
+			cells[cellProcess].north = allWalls[Mathf.RoundToInt((childProcess+(xSize+1)*ySize)+xSize-1)]; 
+		}
+
+		CreateMaze ();
+	}
+
+	void CreateMaze(){
+		while (visitedCells < totalCells) {
+			if (startedBuilding) {
+				GiveMeNeighbour ();
+				if (cells [currentNeighbour].visited == false && cells[currentCell].visited == true) {
+					BreakWall ();
+					cells [currentNeighbour].visited = true;
+					visitedCells++;
+					lastCells.Add (currentCell);
+					currentCell = currentNeighbour;
+					if (lastCells.Count > 0) {
+						backingUp = lastCells.Count - 1;
+					}
+				}
+			} else {
+				currentCell = Random.Range (0, totalCells);
+				cells [currentCell].visited = true;
+				visitedCells++;
+				startedBuilding = true;
+			}
+
+		}
+	}
+
+	void BreakWall(){
+		switch (wallToBreak) {
+		case 1:
+			Destroy (cells [currentCell].north);
+			break;
+		case 2:
+			Destroy (cells [currentCell].east);
+			break;
+		case 3:
+			Destroy (cells [currentCell].west);
+			break;
+		case 4:
+			Destroy (cells [currentCell].south);
+			break;
+		}
+	}
+
+	void GiveMeNeighbour(){
+
+		int length = 0;
+		int[] neighbours = new int[4];
+		int[] connectingWall = new int[4];
+		int check = 0;
+		check = Mathf.RoundToInt(((currentCell+1)/xSize));
+		check -= 1;
+		check *= Mathf.RoundToInt(xSize);
+		check += Mathf.RoundToInt(xSize);
+
+
+		//west
+		if (currentCell + 1 < totalCells && (currentCell + 1) != check) {
+			if (cells [currentCell + 1].visited == false) {
+				neighbours [length] = currentCell + 1;
+				connectingWall [length] = 3;
+				length++;
+			}
+		}
+
+		//east
+		if (currentCell - 1 >= 0 && currentCell != check) {
+			if (cells [currentCell - 1].visited == false) {
+				neighbours [length] = currentCell - 1;
+				connectingWall [length] = 2;
+				length++;
+			}
+		}
+
+		//north
+		if (currentCell + xSize < totalCells) {
+			if (cells [Mathf.RoundToInt(currentCell + xSize)].visited == false) {
+				neighbours [length] = Mathf.RoundToInt(currentCell + xSize);
+				connectingWall [length] = 1; 
+				length++;
+			}
+		}
+
+		//south
+		if (currentCell - xSize >= 0) {
+			if (cells [Mathf.RoundToInt(currentCell - xSize)].visited == false) {
+				neighbours [length] = Mathf.RoundToInt(currentCell - xSize);
+				connectingWall [length] = 4;
+				length++;
+			}
+		}
+
+		if (length != 0) {
+			int theChosenOne = Random.Range (0, length);
+			currentNeighbour = neighbours [theChosenOne];
+			wallToBreak = connectingWall[theChosenOne];
+		} else {
+			if (backingUp > 0) {
+				currentCell = lastCells [backingUp];
+				backingUp--;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
