4e4047a3213d45c50ae7bc934b271716b2bb035f
Jani Kärkkäinen <karkkainen91@gmail.com>
2016-12-10 21:11:19 +0200

    Traps and Objects update
    There is test scene for a demo

---
+++ Assets/Scripts/Environment/TrapMechanic.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapMechanic : MonoBehaviour {
+    //public float m_TrapDelay = 1f;          //not working  as intended ????
+    public float y1 = -1.02f;               //lowest point that object has when moving on y axis
+    public float y2 = 0.63f;                //highest point that object has when moving on y axis
+    public int TrapStepsOnSecond = 30;      //how much trap moves every frame
+    public bool trapFlag = true;            //set false to start trap from up position. true to start from bottom.
+
+    private float x;
+    private float z;
+    private float yDelta;
+    private float y;
+    private WaitForSeconds m_TrapWait;
+    
+
+    void Start()
+    {
+        yDelta = y2 - y1;
+        //m_TrapWait = new WaitForSeconds(m_TrapDelay);//????
+        x = transform.position.x;
+        z = transform.position.z;
+
+        if(trapFlag)
+        {
+            transform.position = new Vector3(x, y1, z);
+        }
+        else
+        {
+            transform.position = new Vector3(x, y2, z);
+        }
+    }
+
+    void Update()
+    {
+        if (trapFlag == true)
+        {
+            if (transform.position.y >= y2)
+            {
+                trapFlag = false;
+            }
+            else
+            {
+                //StartCoroutine(TrapUp());
+                trapUp();
+            }
+        }
+
+        else if (trapFlag == false)
+        {
+            if (transform.position.y <= y1)
+            {
+                trapFlag = true;
+            }
+            else
+            {
+                //StartCoroutine(TrapDown());
+                trapDown();
+            }
+
+        }
+    }
+
+
+    //private IEnumerator TrapUp()
+    private void trapUp()
+    {
+        y = transform.position.y + (yDelta / TrapStepsOnSecond * (Time.deltaTime * 10)); 
+        transform.position = new Vector3(x, y, z);
+        Debug.Log("up");
+        //yield return m_TrapWait;//???
+    }
+
+    //private IEnumerator TrapDown()
+    private void trapDown()
+    {
+        y = transform.position.y - (yDelta / TrapStepsOnSecond * (Time.deltaTime * 10));
+        transform.position = new Vector3(x, y, z);
+        Debug.Log("down");
+        //yield return m_TrapWait;//???
+    }
+}
---
+++ Assets/Scripts/Player/CubePicking.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CubePicking : MonoBehaviour {
+
+    public int numberOfPicups;
+    public Text countText;
+
+    private Rigidbody rb;
+    private int count;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        countText.text = "";
+        count = numberOfPicups;
+        setText();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            setText();
+            if(count == 0)
+            {
+                countText.text = "Ending is now open";
+            }
+        }
+
+        if (other.gameObject.CompareTag("Finish") && count == 0)
+        {
+            //endgame
+            Debug.Log("The end");
+            countText.text = "The end.";
+        }
+    }
+
+    void setText()
+    {
+        countText.text = "Number of objectives left: " + count.ToString();
+    }
+}
