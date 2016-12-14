5f7f50d471e971b4ddd21f002b4a5107b68e7c48
Jani Kärkkäinen <karkkainen91@gmail.com>
2016-12-14 10:39:15 +0200

    spike prefab and audio

--- Assets/Scripts/Player/TrapInteraction.cs
+++ Assets/Scripts/Player/TrapInteraction.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrapInteraction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+public class TrapInteraction : MonoBehaviour {
+    public AudioSource m_DamageAudio;
+    public AudioClip m_Damage1;
+    public AudioClip m_Damage2;
+    int rand;
+    // Use this for initialization
+    void Start () {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,9 +19,27 @@ public class TrapInteraction : MonoBehaviour {
 
     void OnTriggerStay(Collider other)
     {
+        rand = Random.Range(1, 3);
+        //Debug.Log(rand);
+
         if (other.gameObject.CompareTag("Trap"))
         {
-            gameObject.SetActive(false);
+
+            if (!m_DamageAudio.isPlaying)
+            {
+                if(rand == 1)
+                {
+                    m_DamageAudio.clip = m_Damage1;
+                }
+                else if(rand == 2)
+                {
+                    m_DamageAudio.clip = m_Damage2;
+                }
+                m_DamageAudio.pitch = Random.Range(0.5f, 1.5f);
+                m_DamageAudio.Play();
+            }
+
+            //gameObject.SetActive(false);
         }
     }
 }
