7e873b2497e56ca62b51c7d3ce1a64853bb7f642
Atte Visuri <t2viat00@students.oamk.fi>
2016-12-15 15:12:58 +0200

    bullet networking added to mainbranch

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+	[SyncVar(hook = "OnChangedHealth")]
+	public int currentHealth = maxHealth;
+	public RectTransform healthbar;
+
+	public void TakeDamage(int amount)
+	{
+		if(!isServer)
+		{
+			return;
+		}
+
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = maxHealth;
+			Debug.Log("Dead!");
+			RpcRespawn ();
+		}
+	}
+
+	void OnChangedHealth(int health) {
+		healthbar.sizeDelta = new Vector2 (health, healthbar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if(isLocalPlayer)
+		{
+			//set actual spawn position here
+			transform.position = Vector3.zero;
+		}
+	}
+}
